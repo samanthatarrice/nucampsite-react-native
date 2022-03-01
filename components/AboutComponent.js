b0ecbb8e917a4c7adfb452ea5e75ff9ae2243296
Samantha Tarrice <s.tarrice@gmail.com>
2022-03-01 19:01:45 -0300

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, FlatList } from "react-native";
 import { Card, Text, ListItem } from "react-native-elements";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -23,15 +24,36 @@ class About extends Component {
         <ListItem
            title={item.name}
            subtitle={item.description}
-           leftAvatar={{uri: baseUrl + item.image}}
+           leftAvatar={{source: {uri: baseUrl + item.image}}}
          />
       );
     };
 
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title='Community Partners'>
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title='Community Partners'>
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
+
     return (
       <ScrollView>
         <Mission />
-        <Card title={"Community Partners"}>
+        <Card title='Community Partners'>
           <FlatList
             data={this.props.partners.partners}
             renderItem={renderPartner}
