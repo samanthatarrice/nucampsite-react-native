6d114051d620b56a3f0f00dc2d7eb1b5ee231bbd
Samantha Tarrice <s.tarrice@gmail.com>
2022-03-21 13:50:40 -0300

    Workshop 4 Assignment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,3 +1,13 @@
+// import React, { Component } from 'react';
+// import { View, StyleSheet, ScrollView, Image } from 'react-native';
+// import { Input, CheckBox, Button, Icon } from 'react-native-elements';
+// import * as SecureStore from 'expo-secure-store';
+// import * as ImagePicker from 'expo-image-picker';
+// import * as Permissions from 'expo-permissions';
+// import * as ImageManipulator from 'expo-image-manipulator';
+// import { SaveFormat } from 'expo-image-manipulator';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { baseUrl } from '../shared/baseUrl';
 import React, { Component } from 'react';
 import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Input, CheckBox, Button, Icon } from 'react-native-elements';
@@ -6,6 +16,9 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
+import { SaveFormat } from 'expo-image-manipulator';
 
 
 class LoginTab extends Component {
@@ -160,9 +173,34 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({imageUrl: capturedImage.uri});
+        this.processImage(capturedImage.uri)
+        MediaLibrary.saveToLibraryAsync(capturedImage.uri)
       }
     }
+  }
+
+  getImageFromGallery = async () => {
+    const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+    const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1]
+      });
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage);
+        this.processImage(capturedImage.uri)
+      }
+    }
+  }
+
+  processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(imgUri, 
+      [{resize: { width: 400} }], SaveFormat.PNG = 'png');
+
+    console.log(processedImage);
+    this.setState({ imageUrl: processedImage.uri });
   }
 
   handleRegister() {
@@ -191,6 +229,10 @@ class RegisterTab extends Component {
               source={{uri: this.state.imageUrl}}
               loadingIndicatorSource={require('./images/logo.png')}
               style={styles.image}
+            />
+            <Button
+              title='Gallery'
+              onPress={this.getImageFromGallery}
             />
             <Button
               title='Camera'
